1d169f45cb114ae85baf8f2df41b1d2dcb31ea19
Petros Kouzoupis <peterkuzu@gmail.com>
2025-01-26 18:32:11 +0200

    Base project complete
    To add: increase likes on clicking the like button or double clicking the image

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+import { posts } from "./data.js"
+
+const postsArray = posts
+const postContent = document.getElementById("post-content")
+
+for (let post of postsArray) {
+
+    postContent.innerHTML += `
+    <div class="post-content">
+        <div class="user-info">
+            <img src=${post.avatar} alt="" class="my-avatar"/>
+            <div class="user-info-text">
+                <p class="bold">${post.name}</p>
+                <p>${post.location}</p>
+            </div>
+        </div>
+        <img src=${post.post} class="post-img"/>
+        <div class="post-body">
+            <div class="post-icons">
+            <button><img src="./images/icon-heart.png" /></button>
+            <button><img src="./images/icon-comment.png" /></button>
+            <button><img src="./images/icon-dm.png" /></button>
+            </div>
+            <p class="bold">${post.likes} likes</p>
+            <p><span class="bold">${post.username}</span> ${post.comment}</p>
+        </div>
+    </div>
+    `
+}
